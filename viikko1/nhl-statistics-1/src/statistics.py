01095175e2bcbbc8b8fb408036f23bac3af24fb9
Tino Pönni <tino.ponni@tutanota.com>
2021-11-04 18:01:16 +0200

    tasks 14,15 and 16

--- viikko1/nhl-statistics-1/src/statistics.py
+++ viikko1/nhl-statistics-1/src/statistics.py
@@ -6,8 +6,8 @@ def sort_by_points(player):
 
 
 class Statistics:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, reader):
+        #reader = PlayerReader()
 
         self._players = reader.get_players()
 
